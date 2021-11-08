b9fe599918f2cd37349269b8c8f228af0b239aba
LuBeDa <lb@sell-e.de>
2021-11-08 20:12:13 +0100

    Delete awtrix_basics.py

--- awtrix_basics.py
+++
@@ -1,7 +0,0 @@
-work= data.copy()
-
-if work.get("topic","") == "" :
-   work["topic"]="awtrix"
-
-hass.services.call('mqtt', 'publish', { "topic": work["topic"]+"/basic", "payload":str(work) }, False)
-
